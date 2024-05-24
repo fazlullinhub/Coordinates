d826b963b21984a015e494b7b3adc433afd2d812
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-05-24 12:16:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApplication2
+{
+    class Without_metod
+    {
+        static void Main()
+        {
+            Console.Write("Coordinate X1 - ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Coordinate Y1 - ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nCoordinate X2 - ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Coordinate Y2 - ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+            double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("Distance - {O:F}", result);
+
+            Console.ReadKey();
+        }
+    }
+}
